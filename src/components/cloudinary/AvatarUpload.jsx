5a2b4a6ff04919bdda29e5380ca870ef66b30aa2
Let <raulalfonzo66@gmail.com>
2023-04-11 21:04:13 -0400

    Add standard eslinting

--- src/components/cloudinary/AvatarUpload.jsx
+++ src/components/cloudinary/AvatarUpload.jsx
@@ -1,14 +1,12 @@
-import React, {useState, useRef} from 'react'
+import React, { useState, useRef } from 'react'
 import { AiFillDelete, AiFillFileAdd } from 'react-icons/ai'
-
 
 const AvatarUpload = (props) => {
   const [img, setImg] = useState('')
   const inputFileRef = useRef(null)
-  
   const cleanUp = () => {
     URL.revokeObjectURL(img && props.image)
-    inputFileRef.current.value = null;
+    /* inputFileRef.current.value = null; */
   }
   const setImage = (newImage) => {
     if (img) cleanUp()
@@ -18,34 +16,33 @@ const AvatarUpload = (props) => {
     console.log(img)
     const newImage = e.target.files[0]
     console.log(newImage)
-    if(newImage) {
-      setImage( URL.createObjectURL(newImage) )
+    if (newImage) {
+      setImage(URL.createObjectURL(newImage))
     }
     props.imageUpload(e)
   }
 
-
   return (
     <div>
       <img className='w-[200px] m-auto mb-5' src={img} alt='No image yet' />
-      <input 
+      <input
         type='file'
         accept='image/*'
-        style={{ display:'none' }}
+        style={{ display: 'none' }}
         id='imgUpload'
         name='imgUpload'
         ref={inputFileRef}
         onChange={handleOnChange}
       />
 
+      <label
+        className='flex cursor-pointer px-4 py-2 caret-transparent items-center gap-2 h-full w-full    transition-all border-transparent outline-none rounded-md bg-gray-400 hover:bg-gray-300 hover:outline-none hover:border-transparent focus:outline-none'
+        htmlFor='imgUpload'
 
-      <label htmlFor='imgUpload' className='flex cursor-pointer px-4 py-2 caret-transparent items-center gap-2 h-full w-full transition-all border-transparent outline-none rounded-md bg-gray-400 hover:bg-gray-300 hover:outline-none hover:border-transparent focus:outline-none'
-      
       >
-        { img ? <AiFillDelete /> : <AiFillFileAdd /> }
-        { img ? 'Uploaded' : 'Upload' }
+        {img ? <AiFillDelete /> : <AiFillFileAdd />}
+        {img ? 'Uploaded' : 'Upload'}
       </label>
-
 
     </div>
   )
--- src/components/cloudinary/Cloudinary.jsx
+++ src/components/cloudinary/Cloudinary.jsx
@@ -8,45 +8,44 @@ const Cloudinary = () => {
   const [, setImg] = useState({})
 
   const handleImg = (e) => {
-    if(e.target.files[0]){
+    console.log(e)
+    if (e.target.files[0]) {
       setImg({
         src: URL.createObjectURL(e.target.files[0]),
         alt: e.target.files[0].name
       })
+      console.log(e.target.files)
       setLogo(e.target.files[0])
     }
-  } 
+  }
 
   const profileUpload = async (file) => {
     const formData = new FormData()
-    formData.append('file', file)
-    formData.append('upload_preset', 'let_preset')
-    /* formData.append('quality_analysis', true) */
+    formData.set('file', file)
+    formData.set('upload_preset', 'let_preset')
+    /* formData.set('quality_analysis', true) */
     let data = ''
     await Axios.post('https://api.cloudinary.com/v1_1/dadlfo5yu/image/upload', formData).then(
       response => {
         console.log(response)
-        data = response.data['secure_url']
+        data = response.data.secure_url
       }
     ).catch(err => console.log(err.response.data.error))
     return data
   }
 
   const handleSubmit = async (e) => {
-    setImgUpload({...imgUpload, image: logo})
+    setImgUpload({ ...imgUpload, image: logo })
     await profileUpload(logo)
   }
-
-
-
 
   return (
     <div>
       <div className='flex flex-col justify-center gap-4'>
         Cloudinary
-        <AvatarUpload imageUpload={handleImg} image={imgUpload.image} />  
+        <AvatarUpload imageUpload={handleImg} image={imgUpload.image} />
       </div>
-      <button type='submit' onClick={ (e) => { handleSubmit(e) } }>Submit</button>
+      <button type='submit' onClick={(e) => { handleSubmit(e) }}>Submit</button>
     </div>
   )
 }
