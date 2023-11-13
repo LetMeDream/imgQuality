4ccba82a866ab6b244cae0657db7da8f2e6c6ee4
Raul <raulalfonzo66@gmail.com>
2023-11-13 09:36:07 -0400

    Added download option along other details

--- src/components/InputVideoRTC.jsx
+++ src/components/InputVideoRTC.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { RecordRTCPromisesHandler } from 'recordrtc'
+import { RecordRTCPromisesHandler, invokeSaveAsDialog } from 'recordrtc'
 import { IoMdClose } from 'react-icons/io'
 import { useForm, FormProvider } from 'react-hook-form'
 
@@ -9,6 +9,7 @@ const InputVideoRTC = () => {
   const [recorder, setRecorder] = useState()
   const [stream, setStream] = useState()
   const [status, setStatus] = useState(null)
+  const [savedBlob, setSavedBlob] = useState(null)
   // Getting permission on load
   /* const getPermissionInitializeRecorder = async () => {
       try {
@@ -45,7 +46,7 @@ const InputVideoRTC = () => {
 
   const startRecording = async () => {
     const vid = document.querySelector('video')
-    vid.controls= false
+    vid.controls = false
     const previewImg = document.querySelector('[data-image="img-preview"]')
     previewImg.classList.remove('d-none')
     try {
@@ -59,6 +60,7 @@ const InputVideoRTC = () => {
       setRecorder(recorder)
       previewImg.classList.add('d-none')
       vid.srcObject = stream
+      console.log(vid)
       setStream(stream)
       recorder.startRecording()
       setStatus(await recorder.getState())
@@ -69,18 +71,21 @@ const InputVideoRTC = () => {
 
   const stopSaveRecording = async () => {
     const vid = document.querySelector('video')
+    const input = document.querySelector('input[type="file"]')
+    const dataTransferer = new window.DataTransfer()
     vid.pause()
     await recorder.stopRecording()
     setStatus(await recorder.getState())
     const blob = await recorder.getBlob()
     // invokeSaveAsDialog(blob, 'new_name.webm')
-
-    const file = new File([blob], 'RTC_file.webm', {
-      type: 'video/mp4'
+    setSavedBlob(blob)
+    const file = new window.File([blob], 'RTC_file.webm', {
+      type: 'video/webm'
     })
+    dataTransferer.items.add(file)
     setValue('RTC_file', file)
-    vid.controls = true
-    vid.currentTime = 0
+    input.files = dataTransferer.files
+
     stopBothVideoAndAudio(stream)
   }
 
@@ -106,28 +111,41 @@ const InputVideoRTC = () => {
       console.log(video)
       await recorder.destroy()
       setStatus(null)
+      setValue('RTC_file', '')
+      setSavedBlob(null)
       stopBothVideoAndAudio(stream)
     }
   }
 
   const onSubmit = (data) => {
     console.log(data)
-    console.log(errors)
+    window.alert(JSON.stringify(data.RTC_file.name))
   }
 
   useEffect(() => {
     console.log(errors)
   }, [errors])
 
+  const handleFileChange = (e) => {
+    console.log(e)
+  }
+
+  const downloadFile = () => {
+    invokeSaveAsDialog(savedBlob, 'saved.mp4')
+  }
+
+  const styles = {
+    btn: 'bg-gray-200'
+  }
   return (
     <div className='App flex flex-col'>
       Calm down
       <FormProvider {...methods}>
-        <div className='videoContainer border min-w-[220px] border-blue-400 relative'>
-          <video muted className='w-[520px] h-[260px]' />
+        <div className='videoContainer border border-blue-400 relative'>
+          <video autoPlay muted className='w-[520px] max-w-[90vw] h-[260px]' />
           <div
             data-image='img-preview'
-            className='cursor-pointer z-20 bg-white flex justify-center items-center border-red-400 h-[260px] w-[100%] absolute top-0 left-0'
+            className='z-20 bg-white flex justify-center items-center border-red-400 h-[260px] w-[100%] absolute top-0 left-0'
           >
             Preview Image
           </div>
@@ -137,15 +155,18 @@ const InputVideoRTC = () => {
         </div>
         <div className='flex gap-2'>
           {(!status || status === 'stopped' || status === 'destroyed') && <button onClick={startRecording} className='border'>Start</button>}
-          {status && <button onClick={stopSaveRecording} className='border'>Stop</button>}
+          {(status === 'recording' || status === 'paused') && <button onClick={stopSaveRecording} className='border'>Stop</button>}
           {status === 'recording' && <button onClick={pauseRecording} className='border'>Pause</button>}
           {status === 'paused' && <button onClick={resumeRecording} className='border'>Resume</button>}
         </div>
         {status && <div>Status: {status}</div>}
-        <form className='flex flex-col gap-4' onSubmit={handleSubmit(onSubmit)}>
-          <input type='file' {...register('RTC_file', { required: true })} />
-          <button type='submit' className='border'>
+        <form className='flex flex-col md:flex-row items-center gap-1' onSubmit={handleSubmit(onSubmit)}>
+          <input onChange={handleFileChange} type='file' {...register('RTC_file', { required: true })} />
+          <button type='submit' className={styles.btn}>
             Submit
+          </button>
+          <button type='button' className={styles.btn} onClick={downloadFile}>
+            Download
           </button>
         </form>
       </FormProvider>
